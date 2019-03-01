b8ef24f86ad2e06606528c75933fe9e0d0dcb3db
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-03-01 11:51:46 -0500

    Set up React Router in index.js and App.js. Added Navigation component which links from home page to smurf-form

--- village/src/App.js
+++ village/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -25,8 +26,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route path="/" exact render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+        <Route path="/smurf-form" component={SmurfForm} />
       </div>
     );
   }
